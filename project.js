49fc551238f48fae65fbbfb510bc2251184e1fc8
Nayan17107 <prajapatinayan17107@gmail.com>
2025-05-19 11:20:10 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,116 @@
+// P-1 => Find the Area of Triangle.
+
+
+// let area, base = 10, height = 5;
+
+// area = base * height * 0.5;
+
+// console.log("Area of Triangle is =>",area);
+
+
+// P-2 => Convert temperatures to and celcuis into fehernhit.
+
+
+// let Fahrenheit, Celsius = 30;
+
+// Fahrenheit = (Celsius * 9/5) + 32;
+
+// console.log("Fehernhit is =>", Fahrenheit);
+
+
+// P-3 => Calculate the area of Rectangel.
+
+
+// let area, length = 10, width = 5;
+
+// area = length * width;
+
+// console.log("Area of Rectangel is =>", area);
+
+
+// P-4 => Calculate the area of circle.
+
+
+// let area, π = 3.1416, radius = 20;
+
+// area = π * (10 ** 2);
+
+// console.log("Area of circle is =>", area);
+
+
+// P-5 => Convert feet into inches.
+
+
+// let Inches, feet = 20;
+
+// Inches = feet * 12;
+
+// console.log("The Total Inches is =>", Inches);
+
+
+// p-6 => Write a program to calculate given formulas.
+
+
+// A)
+
+// let a = 5, b = 10;
+
+// leftside = a**2 - b**2;
+// rightside = (a-b) * (a+b);
+// Ans = leftside == rightside;
+
+// console.log("leftSide =>", leftside);
+// console.log("RightSide =>", rightside);
+// console.log("Ans is =>", Ans);
+
+
+// B)
+
+// let a = 10, b = 5;
+
+// leftside = (a-b)**2;
+// rightside = a**2 - (2*a*b) + b**2;
+// Ans = leftside == rightside; 
+
+// console.log("leftSide =>", leftside);
+// console.log("RightSide =>", rightside);
+// console.log("Ans is =>", Ans);
+
+
+// C)
+
+// let a = 3; b = 4; c = 5;
+
+// leftside = (a + b + c)**2;
+// rightside = a**2 + b**2 + c**2 + (2*a*b) + (2*a*c) + (2*b*c);
+// Ans = leftside == rightside;
+
+// console.log("leftSide =>", leftside);
+// console.log("RightSide =>", rightside);
+// console.log("Ans is =>", Ans);
+
+
+// D)
+
+// let a = 6; b = 4; c = 5;
+
+// leftside = (a - b - c)**2;
+// rightside = a**2 + b**2 + c**2 - (2*a*b) - (2*a*c) + (2*b*c);
+// Ans = leftside == rightside;
+
+// console.log("leftSide =>", leftside);
+// console.log("RightSide =>", rightside);
+// console.log("Ans is =>", Ans);
+
+
+// E)
+
+// let a = 5, b = 2;
+
+// leftside = (a - b)**3
+// rightside = a**3 - (3 * a**2 * b) + (3 * a * b**2) - b**3
+// Ans = leftside == rightside;
+
+// console.log("leftSide =>", leftside);
+// console.log("RightSide =>", rightside);
+// console.log("Ans is =>", Ans);
