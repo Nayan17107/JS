cf2a6205af92e626201600317143438cbf478c19
Nayan17107 <prajapatinayan17107@gmail.com>
2025-05-31 10:23:39 +0530

    Add files via upload

---
+++ PR-4.js
@@ -0,0 +1,104 @@
+// 1) WAP to Print the Good Morning Message.
+
+// function message() {
+//     console.log("Good Moirning");
+// }
+// message()        ->output = Good Morning 
+
+
+// 2) WAP to find the area of Circle.
+
+// function circle(pi,r)
+// {
+//     let ans = pi * r *r
+//     console.log("The Area of Circle is :",ans);
+// }
+// circle(10,3)         ->output = The Area of Circle is : 90
+
+
+// 3) WAP to find Triangle Area.
+
+// function triangle(l,h)
+// {
+//     let ans = (l * h) / 2
+//     console.log("The Area of Triangle is :",ans);
+// }
+// triangle(20,3)       ->output = The Area of Triangle is : 30
+
+
+// 4) WAP to find area of Rectangle.
+
+// function rectangle()
+// {
+//     let l = 15, h = 5;
+//     let ans = l * h
+//     return ans;
+// }
+// console.log("The Area of Rectangle is :",rectangle());          ->output = The Area of Rectangle is : 90
+
+
+// 5) WAP to find the ans.
+
+// function calculation(a,b,c)
+// {
+//     let ans = ((b * b) * (4 * a * c) / (2 * a));
+//     return ans;
+// }
+// console.log("The Total is :",calculation(5,4,6));        ->output = The Total is : 192
+
+
+// 6) WAP to find the ans.
+
+// function calculation(a,b)
+// {
+//     let Total = ((a * a) + (2 * a * b) + (b *b));
+//     return Total;
+// }
+// console.log("The Ans is :",calculation(10,15));      ->output = The Total is : 625
+
+
+// 7) WAP to Fahrenheit to Celsius.
+
+// function convert(f)
+// {
+//     let c = (f - 32) / 1.8
+//     return c;
+// }
+// console.log("The Total Celsius is :",convert(50));          ->output = The Total Celsius is : 10
+
+
+// 8) WAP to Celsius to Fahrenheit.
+
+// function convert(c)
+// {
+//     let f = (c * 1.8) + 32
+//     return f;
+// }
+// console.log("The Total Fahrenheit is :",convert(10));           ->output = The Total Fahrenheit is :50
+
+
+// 9) WAP to Find given Number is ODD or EVEN.
+
+// function Num(i)
+// {
+//     if (i % 2 === 0) {
+//         console.log("The Number is ODD.");
+//     } else {
+//         console.log("The Number is EVEN.");
+//     }
+// }
+// Num(13);        ->output = 13 is not / 2 so that's why it is Even.
+
+
+// 10) WAP to swap the Value Using Third Variable.
+
+// function swap(a, b) {
+//     console.log("Before swap: a =", a, ", b =", b);
+
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+
+//     console.log("After swap: a =", a, ", b =", b);
+// }
+// swap(5, 15);
