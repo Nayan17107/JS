960e1f9056ad1c54f561408d6adc5dbea881a297
Nayan17107 <prajapatinayan17107@gmail.com>
2025-06-19 18:16:16 +0530

    Add files via upload

---
+++ PR-7.js
@@ -0,0 +1,80 @@
+// // 1) WAP to Print the all the Details.
+
+// class School {
+//     student(name, email, contact) {
+//         console.log("Student Details:");
+//         console.log(name);
+//         console.log(email);
+//         console.log(contact);
+//     }
+
+//     faculty(name, email, contact) {
+//         console.log("Faculty Details:");
+//         console.log(name);
+//         console.log(email);
+//         console.log(contact);
+//     }
+// }
+
+// let Details = new School();
+
+// // Details.student("Nayan", "prajapatinayan@gmail.com", "9714427555");
+
+// // Details.faculty("Girish Sir", "girish@123.com", "9876543210");
+
+
+// // 2) WAP to Print out Person Details
+
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     Details() {
+//         console.log("Name: " + this.name);
+//         console.log("Age: " + this.age);
+//     }
+// }
+
+// class Employee extends Person {
+//     constructor(name, age) {
+//         super(name, age); 
+//     }
+// }
+
+// let N = new Employee("Nayan", 19);
+
+// N.Details();
+
+
+// 3) WAP to Print Simple Message
+
+// class Employee {
+//     constructor(){
+//         console.log("Hello World How Are You All!!!");
+//     }
+// }
+
+// let i = new Employee;
+
+
+// 4) WAP to Calculate the range of circle and Rectangle.
+
+// class shape{
+//     circle(r){
+//         console.log("Area of circle is :-",3.14 * r * r);
+//     }
+
+//     rectangle(width,height){
+//         console.log("Area of rectangle is :-",width * height);
+//     }
+// }
+
+// let i = new shape;
+
+// i.circle("12");
+
+// let j = new shape;
+
+// j.rectangle("5","3")
